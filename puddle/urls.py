527defacff9c47e5fdfb31d915ba40a78e404d4d
linwangboi <linwangboi@gmail.com>
2025-12-11 11:42:35 +0000

    .

--- puddle/urls.py
+++ puddle/urls.py
@@ -18,9 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import index, contact
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name='contact')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
