ed98446ac7bf759e24237682f41c47aeb6729b48
linwangboi <linwangboi@gmail.com>
2025-12-11 11:16:44 +0000

    .

--- puddle/settings.py
+++ puddle/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'core',
+    'item',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+###
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
