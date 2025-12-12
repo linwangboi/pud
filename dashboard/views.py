ea80b9e2e95a6c58446541e8fef2cbe4c8485a20
linwangboi <linwangboi@gmail.com>
2025-12-12 14:43:55 +0000

    .

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.contrib.auth.decorators import login_required
 
@@ -10,5 +10,7 @@ def index(request):
     items = Item.objects.filter(created_by=request.user)
     return render(request, 'dashboard/index.html', {
         'items': items,
-        
     })
+
+
+
--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
     item =  get_object_or_404(Item, pk=pk)
@@ -22,9 +22,27 @@ def new(request):
             item.created_by = request.user
             item.save()
             return redirect('item:detail', pk=item.id)
-
-
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New item',
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    form = EditItemForm(instance=item)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            item = form.save()
+            return redirect('item:detail', pk=item.id)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
