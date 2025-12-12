19d73c0f17e5819a4dfc42e08216b422f48cd11f
linwangboi <linwangboi@gmail.com>
2025-12-12 09:43:27 +0000

    .

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Category, Item
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,14 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
  
+
+def signup(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:index')
+    return render(request, 'core/signup.html', {
+        'form': form,
+    })
